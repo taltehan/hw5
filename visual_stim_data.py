46727fab6b37bcc92df6ce3a387f63b9079c3604
Tal <taltehan@gmail.com>
2018-05-20 17:14:00 +0300

    changes in mock_stim_data, added attrs

--- visual_stim_data.py
+++ visual_stim_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import xarray as xr
 
 
@@ -12,7 +13,9 @@ class VisualStimData:
          ...
     """
     def __init__(self, data, ):
-        pass
+        self.data = data
+        # experimenters, how many rats, some means
+        # ToDo: pre/during/post
 
     def plot_electrode(self, rep_number: int, rat_id: int, elec_number: tuple=(0,)):
         """
@@ -28,10 +31,32 @@ class VisualStimData:
 
 def mock_stim_data() -> VisualStimData:
     """ Creates a new VisualStimData instance with mock data """
-    pass
+    num_of_rats = np.random.randint(10,21)
+    dims = ('electrode', 'time', 'repetition')
+    coords = {'electrode': np.arange(10), 'time': np.linspace(0, 2, num=10000), 'repetition': np.arange(4)}
+    rat_ids = list(range(num_of_rats))
+    experimenter_name = ['Leonardo', 'Donatello', 'Michelangelo', 'Raphael']
+    genders = ['F', 'M']
+    rats_arrays = {}
+    for rat in rat_ids:
+        attrs = {'Rat ID': rat,
+                 'Room temp': np.random.randint(20, 30),
+                 'Room humidity': np.random.randint(30, 70),
+                 'Experimenter name': np.random.choice(experimenter_name),
+                 'Rat gender': np.random.choice(genders)
+                 }
+        rats_arrays[rat] = xr.DataArray(np.random.random((len(coords['electrode']),
+                                                          len(coords['time']),
+                                                          len(coords['repetition']))),
+                                        dims=dims, coords=coords, attrs=attrs
+                                        )
+    rats_ds = VisualStimData(data=rats_arrays)
+    return rats_ds
 
 
 if __name__ == '__main__':
-    stim_data = mock_stim_data()
-    stim_data.plot_electrode()  # add necessary vars
-    stim_data.experimenter_bias()
+    # stim_data = mock_stim_data()
+    # stim_data.plot_electrode()  # add necessary vars
+    # stim_data.experimenter_bias()
+    # ToDo: remove comments above
+    print(mock_stim_data().data)
