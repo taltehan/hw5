cf27bfbbab486926efe4002b5e52bac9ef48d932
Tal <taltehan@gmail.com>
2018-05-20 23:25:34 +0300

    minor updates - comments+plots

--- visual_stim_data.py
+++ visual_stim_data.py
@@ -8,19 +8,19 @@ class VisualStimData:
     Data and methods for the visual stimulus ePhys experiment.
     The data table itself is held in self.data, an `xarray` object.
     Inputs:
-        data: xr.DataArray or xr.Dataset
-        ...
+        data: xr.Dataset
     Methods:
-         ...
+         plot_electrode(rep_number, rat_id, elec_number) - plots the voltage of the selected electrodes for a specific
+                                                           rat and repetition number.
+         experimenter_bias - plots the mean, std and median voltage across different experimenters.
     """
-    def __init__(self, data, ):
+    def __init__(self, data):
         self.data = data
-        # experimenters, how many rats, some means
-        # ToDo: pre/during/post
         self.vars = self.data.data_vars
         self.experimenters = list(set([self.data[da].attrs['experimenter_name'] for da in self.vars]))
         self.num_rats = len(self.vars)
         self.num_repetitions = len(self.data.repetition)
+        # Stimulus Index: division of the data by pre- post- and during-stimulus times:
         self.pre_stimulus = self.data.where(self.data.time < 1, drop=True)
         self.post_stimulus = self.data.where(self.data.time > 1.1, drop=True)
         self.after_stimulus = self.data.where(self.data.time >= 1, drop=True)
@@ -49,21 +49,22 @@ class VisualStimData:
         x = np.arange(3)
         labels = ('mean', 'std', 'median')
         width = 0
+        # get the data of each experimenter and plot the mean, std and median
         for experimenter in self.experimenters:
             exp_data = self.data.filter_by_attrs(experimenter_name=experimenter)
             vals = [exp_data[da].values for da in exp_data.data_vars]
             y = [np.mean(vals), np.std(vals), np.median(vals)]
-            ax.bar(x + width, y, 0.3, label=f'{experimenter}')
-            width+=0.2
+            ax.bar(x + width, y, 0.2, label=f'{experimenter}')
+            width += 0.2
         plt.xticks(x, labels)
-        ax.set_xticks(np.arange(3) + 0.3 / 2)
+        ax.set_xticks(np.arange(3) + 0.5 / 2)
         ax.legend()
         plt.show()
 
 
 def mock_stim_data() -> VisualStimData:
     """ Creates a new VisualStimData instance with mock data """
-    num_of_rats = np.random.randint(10,21)
+    num_of_rats = np.random.randint(10, 21)
     dims = ('electrode', 'time', 'repetition')
     coords = {'electrode': np.arange(10), 'time': np.linspace(0, 2, num=10000), 'repetition': np.arange(4)}
     rat_ids = list(range(num_of_rats))
